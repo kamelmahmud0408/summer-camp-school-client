4dd9c46b36147a9b017c32fad43952e2aaddbcb4
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-06-08 01:50:57 +0600

    added footer section

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet} from "react-router-dom";
 import Navbar from '../pages/Sheared/Navbar/Navbar';
+import Footer from '../pages/Sheared/Footer/Footer';
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
