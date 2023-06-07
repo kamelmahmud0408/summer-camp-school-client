fbbfa35628d46791b3ad482fe2b09abfd031a731
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-06-08 01:02:35 +0600

    project set up and added navbar

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '../../../components/Container';
+
+const Home = () => {
+    return (
+        <div>
+            <Container>
+                <h3>this is homererffgg</h3>
+            </Container>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Sheared/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Container from '../../../components/Container';
+
+const Navbar = () => {
+    const navItem = <>
+        <li>home</li>
+        <li>home</li>
+        <li>home</li>
+    </>
+    return (
+        <Container>
+            <div className="navbar fixed z-10 bg-opacity-30 max-w-screen-xl bg-black text-white">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {navItem}
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost normal-case text-2xl">Summer School</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navItem}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Get started</a>
+                </div>
+            </div>
+        </Container>
+    );
+};
+
+export default Navbar;
