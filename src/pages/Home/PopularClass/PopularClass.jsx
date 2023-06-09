3d96ebbee3af28efa7ac4af46b5854d8d94ad2da
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-06-10 03:12:14 +0600

    use sort and limit data on top classes page

--- src/pages/Home/PopularClass/PopularClass.jsx
+++ src/pages/Home/PopularClass/PopularClass.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 const PopularClass = () => {
     const [popular, setpopular] = useState([])
     useEffect(() => {
-        fetch('classes.json')
+        fetch('http://localhost:5000/classes')
             .then(res => res.json())
             .then(data => {
                 setpopular(data)
@@ -14,13 +14,14 @@ const PopularClass = () => {
     return (
         <div className='grid grid-cols-3 gap-5'>
             {
-                popular.map(item => <>
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                        <figure><img src={item.image} alt="" /></figure>
+                popular.slice(0,6).map(item => <>
+                    <div className="card w-full bg-base-100 shadow-xl">
+                        <figure><img className='h-72' src={item.image} alt="" /></figure>
                         <div className="card-body">
                             <h2 className="card-title">{item.name}</h2>
                             <p>{item.instructor}</p>
-                            <p>{item.price}</p>
+                            <p>${item.price}</p>
+                            <p>{item.number_of_students}</p>
                             <div className="card-actions">
                                 <button className="btn btn-primary">Buy Now</button>
                             </div>
