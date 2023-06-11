feef2608028cb9e63cbf686c72b0b854ac8f01fd
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-06-11 19:23:06 +0600

    added jwt token on local storage and implement axios

--- src/pages/Dashboard/AdminPage/ManageUser.jsx
+++ src/pages/Dashboard/AdminPage/ManageUser.jsx
@@ -1,11 +1,39 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Swal from 'sweetalert2';
 
 const ManageUser = () => {
     const { data: users = [], refetch } = useQuery(['users'], async () => {
         const res = await fetch('http://localhost:5000/users')
         return res.json();
     })
+
+    const handleAdmin = user => {
+        console.log(user)
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.modifiedCount) {
+                    refetch()
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: `${user.name} is admin now`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            })
+    }
+
+    
+    const handleInstructor=user=>{
+
+    }
+
     return (
         <>
             <h3>All user: {users.length}</h3>
@@ -25,12 +53,13 @@ const ManageUser = () => {
                     </thead>
                     <tbody>
                         {users.map((user, index) => <tr key={user._id}>
-                            <td>{index+1}</td>
+                            <td>{index + 1}</td>
                             <td><img className='w-14 h-14 rounded-full' src={user.image} alt="" /></td>
                             <td>{user.name}</td>
                             <td>{user.email}</td>
-                            <td>Admin</td>
-                            <td>Instructor</td>
+                            <td>{user.role==='admin' ? 'admin now': <button onClick={() => handleAdmin(user)} className='btn btn-error btn-sm'>Admin</button>
+                                }</td>
+                            <td><button onClick={() => handleInstructor(user)} className='btn btn-error btn-sm'>Instructor</button></td>
 
                         </tr>)}
 
