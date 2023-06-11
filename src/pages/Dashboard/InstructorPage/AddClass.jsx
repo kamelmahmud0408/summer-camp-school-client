e738caae53a28da024c00493a24e99a59794e135
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-06-12 00:43:25 +0600

    added add class section and send to on server

--- src/pages/Dashboard/InstructorPage/AddClass.jsx
+++ src/pages/Dashboard/InstructorPage/AddClass.jsx
@@ -1,9 +1,145 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../provider/AuthProvider';
+import { useForm } from 'react-hook-form';
+import Swal from 'sweetalert2';
 
 const AddClass = () => {
+
+    // const navigate = useNavigate()
+    // const location = useLocation()
+
+    // const from = location.state?.from?.pathname || '/myclasses'
+
+
+    const { user } = useContext(AuthContext)
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data)
+
+        fetch('http://localhost:5000/class', {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(data)
+
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result)
+                if (result.insertedId) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'class added successfully',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                    })
+                }
+                // navigate(from, { replace: true })
+            })
+    }
+
+
     return (
-        <div>
-            <h3>gsgszg</h3>
+        <div className=' w-full h-full ms-10' >
+            <h1 className='text-center text-3xl text-orange-500 font-bold my-10'>Add a Class</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                {errors.exampleRequired && <span>This field is required</span>}
+
+                <div className='grid grid-cols-2 gap-3'>
+                    <div className='mb-3'>
+                        <h3 className='text-xl font-semibold mb-3'>Class Name</h3>
+
+                        <input
+                            className=" w-full p-2 border"
+                            {...register("className")}
+                            placeholder="Class Name "
+
+                        />
+
+                    </div>
+                    <div>
+                        <h3 className='text-xl font-semibold mb-3'>Class Image</h3>
+                        <input
+                            className="w-full p-2 border"
+                            {...register("image")}
+                            placeholder="image link"
+                            type="url"
+
+                        />
+                    </div>
+
+                    <div className='mb-3'>
+                        <h3 className='text-xl font-semibold mb-3'>Instructor Name</h3>
+
+                        <input
+                            className="w-full p-2 border"
+                            {...register("instructorName")}
+                            placeholder="InstructorName"
+                            defaultValue={user?.displayName}
+
+                        />
+
+                    </div>
+                    <div className='mb-3'>
+                        <h3 className='text-xl font-semibold mb-3'>Instrctor Email</h3>
+
+                        <input
+                            className="w-full p-2 border"
+                            {...register("email")}
+                            placeholder="email"
+                            defaultValue={user?.email}
+
+                        />
+
+                    </div>
+                    <div className='mb-3'>
+                        <h3 className='text-xl font-semibold mb-3'>Price</h3>
+
+                        <input
+                            className="w-full p-2 border"
+                            {...register("price")}
+                            placeholder="Price"
+
+                        />
+
+                    </div>
+                    <div className='mb-3'>
+                        <h3 className='text-xl font-semibold mb-3'>Available Seates</h3>
+
+                        <input
+                            className="w-full p-2 border"
+                            {...register("available_seates")}
+                            placeholder="Available Seates"
+
+                        />
+
+                    </div>
+                    <div className='mb-3'>
+                        <h3 className='text-xl font-semibold mb-3'>Status</h3>
+
+                        <input
+                            className="w-full p-2 border"
+                            {...register("status")}
+                            placeholder="Status"
+
+                        />
+
+                    </div>
+
+                   
+                    
+                </div>
+
+
+                <input className=" btn btn-error mt-5 w-full" value="Add Class" type="submit" />
+            </form>
         </div>
     );
 };
