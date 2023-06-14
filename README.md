8c8e4af0d10603456e6ba751c0dd45509a9a3f87
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-06-14 20:20:08 +0600

    added redme file and some update

---
+++ README.md
@@ -0,0 +1,24 @@
+## EliteSports Academy
+
+* This project is implementation of EliteSports Academy, a sports academy website aimed at providing a platform for users to explore and enroll in various sports classes. The website is responsible for handling the user interface and interaction components of the website, ensuring a smooth and engaging user experience
+* Users can sign up and log in to access personalized features, secure authentication using email and password
+
+* Student Dashboard:Displaying classes selected by the student, including relevant information, delete option, and payment functionality and  showing all the classes in which the student has successfully enrolled
+
+* Instructor Dashboard:Form for instructors to add new classes, including class name, image, available seats, price, and automatic inclusion of instructor details
+
+* Admin Dashboard:Displaying all classes added by instructors, with relevant information, status, and approval/denial functionality and viewing and managing registered users, including the ability to promote users to instructors or admins
+
+
+
+
+## Technology Used
+* React: JavaScript library for building user interfaces
+* React Router: Routing library for handling navigation within the      application
+* React Icons: Library for utilizing icons in the application
+* React Query: Data fetching and caching library
+* Axios: HTTP client for making API requests
+* Tailwind CSS: CSS framework for styling
+* Firebase: Backend as a Service (BaaS) platform for additional functionality
+
+## [EliteSports Academy](https://baby-toys-marketlace.web.app/)
--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import { FaHome, FaWallet } from 'react-icons/fa';
+import { FaHome, FaWallet, FaUserShield, FaChalkboardTeacher,FaUser,FaBookOpen } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 import useAdmin from '../hooks/useAdmin';
 import useInstructor from '../hooks/useInstructor';
@@ -13,51 +13,51 @@ const Dashboard = () => {
 
     return (
         <>
-        <motion.div
-        initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -50 }}
-        transition={{ duration: 0.5 }}
-        
-      >
+            <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -50 }}
+                transition={{ duration: 0.5 }}
 
-            <div className="drawer lg:drawer-open">
+            >
 
-                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content flex flex-col items-center justify-center">
-                    {/* Page content here */}
-                    <Outlet></Outlet>
-                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                <div className="drawer lg:drawer-open">
 
+                    <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+                    <div className="drawer-content flex flex-col items-center justify-center">
+                        {/* Page content here */}
+                        <Outlet></Outlet>
+                        <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+
+                    </div>
+
+                    <div className="drawer-side">
+                        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                        <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content space-y-2">
+                            {/* Sidebar content here */}
+                            {
+                                isAdmin ? <>
+                                    <li><NavLink to='/dashboard/manageclass'> <FaUserShield></FaUserShield> Manage Classes </NavLink></li>
+                                    <li><NavLink to='/dashboard/manageuser'> <FaUserShield></FaUserShield>Manage Users</NavLink></li>
+                                </> : isInstructor
+                                    ? <><li><NavLink to='/dashboard/addclass'> <FaChalkboardTeacher></FaChalkboardTeacher> Add A Class</NavLink></li>
+                                        <li><NavLink to='/dashboard/myclasses'> <FaChalkboardTeacher></FaChalkboardTeacher> My Classes</NavLink></li>
+                                    </> : <>
+                                        <li><NavLink to='/dashboard/myenrolledclass'> <FaUser></FaUser> My Enrolled Class</NavLink></li>
+                                        <li><NavLink to='/dashboard/myselectedclass' > <FaUser></FaUser> My selected Class</NavLink></li>
+                                        <li><NavLink to='/dashboard/history' > <FaWallet></FaWallet> Payment History</NavLink></li>
+                                    </>
+                            }
+
+
+                            <div className="divider">OR</div>
+                            <li><NavLink to='/'><FaHome></FaHome>Home</NavLink></li>
+                            <li><NavLink to='/instructor'><FaChalkboardTeacher></FaChalkboardTeacher> Instructor</NavLink></li>
+                            <li><NavLink to='/classes'><FaBookOpen></FaBookOpen> Classes</NavLink></li>
+                        </ul>
+
+                    </div>
                 </div>
-
-                <div className="drawer-side">
-                    <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content space-y-2">
-                        {/* Sidebar content here */}
-                        {
-                            isAdmin ? <>
-                                <li><NavLink to='/dashboard/manageclass'> <FaHome></FaHome> Manage Classes </NavLink></li>
-                                <li><NavLink to='/dashboard/manageuser'> <FaWallet></FaWallet> Manage Users</NavLink></li>
-                            </> : isInstructor
-                                ? <><li><NavLink to='/dashboard/addclass'> <FaHome></FaHome> Add A Class</NavLink></li>
-                                    <li><NavLink to='/dashboard/myclasses'> <FaHome></FaHome> My Classes</NavLink></li>
-                                </> : <>
-                                    <li><NavLink to='/dashboard/myenrolledclass'> <FaHome></FaHome> My Enrolled Class</NavLink></li>
-                                    <li><NavLink to='/dashboard/myselectedclass' > <FaWallet></FaWallet> My selected Class</NavLink></li>
-                                    <li><NavLink to='/dashboard/history' > <FaWallet></FaWallet> Payment History</NavLink></li>
-                                </>
-                        }
-
-
-                        <div className="divider">OR</div>
-                        <li><NavLink to='/'><FaHome></FaHome>Home</NavLink></li>
-                        <li><NavLink to='/instructor'>Instructor</NavLink></li>
-                        <li><NavLink to='/classes'>Classes</NavLink></li>
-                    </ul>
-
-                </div>
-            </div>
             </motion.div>
         </>
     );
